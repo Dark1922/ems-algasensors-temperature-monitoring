a39f332e9904a5cf20f41f7cc18b7efcbc689d98
João Victor <jvdematos004@gmail.com>
2025-08-15 08:25:32 -0300

    Setup do microsserviço de Temperature Monitoring

---
+++ src/main/java/com/algaworks/algasensors/temperature/monitoring/api/config/jpa/TSIDToLongJPAAttributeConverter.java
@@ -0,0 +1,19 @@
+package com.algaworks.algasensors.temperature.monitoring.api.config.jpa;
+
+import io.hypersistence.tsid.TSID;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class TSIDToLongJPAAttributeConverter implements AttributeConverter<TSID, Long> {
+
+    @Override
+    public Long convertToDatabaseColumn(TSID tsid) {
+        return tsid.toLong();
+    }
+
+    @Override
+    public TSID convertToEntityAttribute(Long value) {
+        return TSID.from(value);
+    }
+}
