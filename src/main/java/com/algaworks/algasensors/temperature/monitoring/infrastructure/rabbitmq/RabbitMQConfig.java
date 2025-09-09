341a2079de3517c6580549c053cfc3a4b8e02917
João Victor <jvdematos004@gmail.com>
2025-09-09 07:06:57 -0300

    13.4. Utilizando uma Dead Letter Queue (DLQ)

--- src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQConfig.java
+++ src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQConfig.java
@@ -8,11 +8,16 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class RabbitMQConfig {
 
-    public static final String QUEUE_PROCESS_TEMPERATURE = "temperature-monitoring.process-temperature.v1.q";
-    private static final String QUEUE_ALERTING = "temperature-monitoring.alerting.v1.q" ;
+    private static final String PROCESS_TEMPERATURE = "temperature-monitoring.process-temperature.v1";
+    public static final String QUEUE_PROCESS_TEMPERATURE = PROCESS_TEMPERATURE + ".q";
+    public static final String DEAD_LETTER_QUEUE_PROCESS_TEMPERATURE = PROCESS_TEMPERATURE + ".dlq";
+    public static final String QUEUE_ALERTING = "temperature-monitoring.alerting.v1.q" ;
 
     //transforma o objeto em json para enviar via rabbitmq
     @Bean
@@ -27,8 +32,17 @@ public class RabbitMQConfig {
 
     @Bean
     public Queue queueProcessTemprature() {
+        Map<String, Object> args = new HashMap<>();
+        args.put("x-dead-letter-exchange", ""); //default exchange
+        args.put("x-dead-letter-routing-key", DEAD_LETTER_QUEUE_PROCESS_TEMPERATURE);
         return  QueueBuilder
-                .durable(QUEUE_PROCESS_TEMPERATURE).build();
+                .durable(QUEUE_PROCESS_TEMPERATURE).withArguments(args).build();
+    }
+
+    @Bean
+    public Queue deadLetterQueueProcessTemprature() {
+        return  QueueBuilder
+                .durable(DEAD_LETTER_QUEUE_PROCESS_TEMPERATURE).build();
     }
 
     @Bean
